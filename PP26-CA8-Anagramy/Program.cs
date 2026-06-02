7f8b165d5ab4a32a440f746aa699c07760aebe51
Mango67 <4422807@TE-GDA-D-D21E03>
2026-06-02 11:44:02 +0200

    Szukanie wierszy z anagramami

--- PP26-CA8-Anagramy/Program.cs
+++ PP26-CA8-Anagramy/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿string[] lines = File.ReadAllLines("anagram.txt");
+
+List<string> anagramLines = new List<string>();
+
+foreach (string line in lines)
+{
+    string[] words = line.Split(' ');
+    char[] chars = words[0].ToCharArray();
+
+    bool isAnagram = true;
+    for (int i = 1; i < words.Length; i++)
+    {
+        if (words[i].Length != words[0].Length)
+        {
+            isAnagram = false;
+            break;
+        }
+
+        char[] testChars = words[i].ToCharArray();
+
+        Array.Sort(chars);
+        Array.Sort(testChars);
+        if (!chars.SequenceEqual(testChars))
+        {
+            isAnagram = false;
+            break;
+        }
+    }
+
+    if (isAnagram)
+    {
+        anagramLines.Add(line);
+    }
+}
+
+foreach (string anagramLine in anagramLines)
+{
+    Console.WriteLine(anagramLine);
+}
+
+File.WriteAllLines("znalezione.txt", anagramLines);
